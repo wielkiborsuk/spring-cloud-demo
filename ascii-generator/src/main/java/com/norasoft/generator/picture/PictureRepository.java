f4f56a4ca896ed87802ac06f954b73f3a967d7d6
wielki.borsuk <wielki.borsuk@gmail.com>
2016-10-23 23:04:51 +0200

    connect to eureka, first attempt at feign client

--- ascii-generator/src/main/java/com/norasoft/generator/picture/PictureRepository.java
+++ ascii-generator/src/main/java/com/norasoft/generator/picture/PictureRepository.java
@@ -9,27 +9,36 @@ import java.util.Map;
 import java.util.Optional;
 import javax.annotation.PostConstruct;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import com.norasoft.generator.picture.Picture;
+import com.norasoft.generator.picture.PictureRepoClient;
 
 @Component
 public class PictureRepository {
 
   private Map<Long, Picture> cache = new HashMap<>();
   private Long counter = 1l;
-  private static final String pictureHost = "localhost";
-  private static final String pictureProtocol = "http";
-  private static final Integer picturePort = 7777;
+  private boolean runOnce = false;
 
-  @PostConstruct
-  private void init() throws MalformedURLException {
-    RestTemplate restTemplate = new RestTemplate();
-    Class<List<String>> clazz = (Class<List<String>>) (Class<?>) List.class;
-    ResponseEntity<List<String>> listResponse = restTemplate.getForEntity(getResourceURL("list").toString(), clazz);
-    List<String> list = listResponse.getBody();
+  @Autowired
+  private PictureRepoClient pictureRepoClient;
+
+  //@PostConstruct
+  private void init() {
+    //RestTemplate restTemplate = new RestTemplate();
+    //Class<List<String>> clazz = (Class<List<String>>) (Class<?>) List.class;
+    //ResponseEntity<List<String>> listResponse = restTemplate.getForEntity(getResourceURL("list").toString(), clazz);
+    //List<String> list = listResponse.getBody();
+    
+    if (runOnce) {
+      return;
+    }
+    runOnce = true;
+
+    List<String> list = pictureRepoClient.listPictures();
 
     for (String filename : list) {
       save(new Picture(getImageName(filename), getResourceURL(filename)));
@@ -37,10 +46,12 @@ public class PictureRepository {
   }
 
   public Optional<Picture> findOne(Long id) {
+    init();
     return Optional.ofNullable(cache.getOrDefault(id, null));
   }
 
   public Iterable<Picture> findAll() {
+    init();
     return cache.values();
   }
 
@@ -61,6 +72,10 @@ public class PictureRepository {
   }
 
   private URL getResourceURL(String path) {
+    String pictureProtocol = "http";
+    Integer picturePort = 7777;
+    String pictureHost = "localhost";
+
     try {
       URI uri = new URI(pictureProtocol, null, pictureHost, picturePort, "/"+path, null, null);
       return uri.toURL();
