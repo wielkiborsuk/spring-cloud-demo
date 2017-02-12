a042c45e3c564982ebde71998825140aebb9031e
wielki.borsuk <wielki.borsuk@gmail.com>
2017-02-12 23:46:00 +0100

    add fallback to the ascii generation method in the frontend

--- frontend/src/main/java/com/norasoft/frontend/picture/AsciiServiceClient.java
+++ frontend/src/main/java/com/norasoft/frontend/picture/AsciiServiceClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Component
-@FeignClient("ascii-generator")
+@FeignClient(name = "ascii-generator", fallback = AsciiServiceFallbackClient.class)
 public interface AsciiServiceClient {
   @RequestMapping(path="/ascii", method = RequestMethod.POST)
   String generateAscii(@RequestBody String url);
