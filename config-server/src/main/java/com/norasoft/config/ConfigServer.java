de501175c4f4fc14467a4acc7eabf76aed86251d
wielki.borsuk <wielki.borsuk@gmail.com>
2017-11-20 02:38:25 +0100

    enable config server

--- config-server/src/main/java/com/norasoft/config/ConfigServer.java
+++ config-server/src/main/java/com/norasoft/config/ConfigServer.java
@@ -3,8 +3,10 @@ package com.norasoft.config;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
 public class ConfigServer {
 
